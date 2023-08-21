65af77c697e790a2967ccda5e12c45af95c3905e
Darkie Lee <ducle0906.ulis@gmail.com>
2023-08-21 23:03:01 +0700

    create app and main

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import NavBar from './components/navbar'
+import Home from './components/home'
+import Market from './components/market'
+import ChooseUs from './components/chooseUs'
+import JoinUs from './components/joinUs'
+import "./styles/styles.css"
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Market />
+      <ChooseUs />
+      <JoinUs />
+    </>
+  )
+}
+
+export default App
